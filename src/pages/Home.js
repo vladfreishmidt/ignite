43b41d291881f36fc2990532c2d3dcc5457705a6
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2021-02-13 11:54:30 +0200

    Game component - added styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,52 @@
 import React, { useEffect } from 'react';
 // Redux
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loadGames } from '../actions/gamesActions';
+// Components
+import Game from '../components/Game';
+// Styling and Animation
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadGames());
-  });
-  return <h1>Home</h1>;
+  }, [dispatch]);
+  // Get that data back
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+
+  return (
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game, idx) => (
+          <Game
+            key={game.id}
+            id={game.id}
+            name={game.name}
+            released={game.released}
+            cover={game.background_image}
+          />
+        ))}
+      </Games>
+    </GameList>
+  );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
